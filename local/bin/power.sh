115ed7c7fe9b006cf46e4ea8f2517e409cfd2a01
Chua Hou <human+github@chuahou.dev>
2020-10-31 19:15:57 +0800

    i3, power.sh: add power management

---
+++ local/bin/power.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020 Chua Hou
+#
+# Alerts when battery at critical level, and executes shutdown at even lower
+# level.
+
+# levels at which to notify, and shutdown
+NOTIFY_LEVEL=10
+SHUTDOWN_LEVEL=5
+
+# delay in seconds before shutting down
+SHUTDOWN_DELAY=15
+
+# interval to check in seconds
+LOOP_INTERVAL=60
+
+while true; do
+
+	# get battery level
+	CURRENT_LEVEL=$(upower -i $(upower -e | grep battery) | \
+		sed -n 's/.*percentage:\s*\([0-9]\+\)%/\1/p')
+
+	# ensure is integer, otherwise notify
+	[[ $CURRENT_LEVEL =~ ^[0-9]+$ ]] || \
+		notify-send -u critical "Invalid battery level $CURRENT_LEVEL%!"
+
+	# notify if lower than NOTIFY_LEVEL
+	[ "$CURRENT_LEVEL" -lt "$NOTIFY_LEVEL" ] && \
+		notify-send -u critical "Battery level critical at $CURRENT_LEVEL%!"
+
+	# notify and shutdown in SHUTDOWN_DELAY seconds if lower than SHUTDOWN_LEVEL
+	[ "$CURRENT_LEVEL" -lt "$SHUTDOWN_LEVEL" ] && \
+		notify-send -u critical \
+			"Shutting down in $SHUTDOWN_DELAY seconds!" && \
+		paplay /usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga && \
+		sleep $SHUTDOWN_DELAY && \
+		systemctl poweroff
+
+	sleep $LOOP_INTERVAL
+
+done
