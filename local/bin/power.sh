f1f9b21513be5095ff431e22024a473f1bfcbd76
Chua Hou <human+github@chuahou.dev>
2020-10-31 20:46:46 +0800

    power.sh: pass name 'power.sh' to notify-send

--- local/bin/power.sh
+++ local/bin/power.sh
@@ -23,15 +23,17 @@ while true; do
 
 	# ensure is integer, otherwise notify
 	[[ $CURRENT_LEVEL =~ ^[0-9]+$ ]] || \
-		notify-send -u critical "Invalid battery level $CURRENT_LEVEL%!"
+		notify-send -u critical -a $(basename $0) \
+			"Invalid battery level $CURRENT_LEVEL%!"
 
 	# notify if lower than NOTIFY_LEVEL
 	[ "$CURRENT_LEVEL" -lt "$NOTIFY_LEVEL" ] && \
-		notify-send -u critical "Battery level critical at $CURRENT_LEVEL%!"
+		notify-send -u critical -a $(basename $0) \
+			"Battery level critical at $CURRENT_LEVEL%!"
 
 	# notify and shutdown in SHUTDOWN_DELAY seconds if lower than SHUTDOWN_LEVEL
 	[ "$CURRENT_LEVEL" -lt "$SHUTDOWN_LEVEL" ] && \
-		notify-send -u critical \
+		notify-send -u critical -a $(basename $0) \
 			"Shutting down in $SHUTDOWN_DELAY seconds!" && \
 		paplay /usr/share/sounds/freedesktop/stereo/phone-incoming-call.oga && \
 		sleep $SHUTDOWN_DELAY && \
