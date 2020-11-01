e5714790d52372715f253c34c61d62ddb4a8cbf4
Chua Hou <human+github@chuahou.dev>
2020-11-01 18:09:21 +0800

    gen_script.sh: autogenerate standalone rcup script for portable use
    - Call on rcpush
    - Ignore in rcrc

---
+++ gen_script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2020 Chua Hou
+#
+# This script creates a standalone script for systems on which I do not have
+# root access to install rcm on.
+
+set -e
+
+cd $(dirname $0)
+RCRC=./rcrc rcup -t none -g > standalone.sh
+sed -i "s@$HOME@\$HOME@g" standalone.sh
+chmod +x standalone.sh
+cd -
