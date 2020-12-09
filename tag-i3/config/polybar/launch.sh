a340f2a5d1369c366cbaaea1ed25cd1ef38f3f44
Chua Hou <human+github@chuahou.dev>
2020-12-09 09:12:40 +0800

    polybar: use IPC for nordvpn module

--- tag-i3/config/polybar/launch.sh
+++ tag-i3/config/polybar/launch.sh
@@ -3,7 +3,7 @@
 killall -q polybar
 polybar main & disown
 
-sleep 5
-for ipc in dnd_ipc sound_ipc cpufreq_ipc; do
+sleep 1
+for ipc in dnd_ipc nordvpn_ipc sound_ipc cpufreq_ipc; do
 	polybar-msg hook $ipc 1; sleep 1
 done
