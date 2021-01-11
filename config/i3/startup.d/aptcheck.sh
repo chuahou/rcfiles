5f504c55ceed9f8736dd4bd596eabe64439dc419
Chua Hou <human+github@chuahou.dev>
2021-01-11 17:52:07 +0800

    step

--- config/i3/startup.d/aptcheck.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2020 Chua Hou
-
-notify-send -a "Updates" \
-	"$(/usr/lib/update-notifier/apt-check --human-readable)"
--- local/bin/lock.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Disables dunst notifications, locks the screen using i3lock, then reenables
-# notifications.
-#
-# Also see https://github.com/dunst-project/dunst/issues/77.
-donotdisturb.sh on
-i3lock --nofork -i "$PIC_PATH"/blur.png "$@"
-donotdisturb.sh off
